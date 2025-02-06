48be6101ea91dac556eba1a68e55d3105f4843cd
dnjak <dnjak0115@icloud.com>
2025-02-06 10:18:29 -0500

    Add files via upload

---
+++ P1LAB1_NjakaDaniel.py
@@ -0,0 +1,8 @@
+ # Daniel Njaka
+ # 2/4/25
+ # P1LAB1
+ # Program allowing user to enter their name
+
+first_name = input("Please enter your first name: ")
+last_name = input ("Please enter your last name: ")
+print(f"Hello, {first_name} {last_name}! Welcome to CTI 110")
